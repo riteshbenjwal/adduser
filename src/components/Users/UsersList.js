ed297cad0af509fd3600662a726ca49e8eaa1c21
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-07-28 11:38:44 +0530

    first commit

---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from "./UsersList.module.css";
+
+function UsersList(props) {
+  return (
+    <div>
+      <Card className={classes.users}>
+        <ul>
+          {props.users.map((user) => (
+            <li key={user.id}>
+              {user.name} ({user.age} years old){" "}
+            </li>
+          ))}
+        </ul>
+      </Card>
+    </div>
+  );
+}
+
+export default UsersList;
